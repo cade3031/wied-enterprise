1affe6721e4744a3a96795f4b4cfb41a89e2901d
root <cadewied@icloud.com>
2024-06-07 17:28:29 +0000

    added homepage btn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Homebtn from './components/Homebtn';
 import NavBar from './components/NavBar';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  <Homebtn />
   <NavBar />,
   </React.StrictMode>,
   document.getElementById('root')
