4c4a43f9e5eafd2ba9c55994754ba29cac302450
root <cadewied@icloud.com>
2024-06-07 17:03:19 +0000

    added homepage btn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Homebtn from './componets/Homebtn';
+import Homebtn from './components/Homebtn';
 
 ReactDOM.render(
   <React.StrictMode>
