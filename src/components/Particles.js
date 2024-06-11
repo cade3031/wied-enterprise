2ead983040e11f4c1452a581e13cfc1ae28d7e9d
Cade Wied <cadewied@Cades-MacBook-Pro.local>
2024-06-11 11:02:13 -0400

    removed changes

---
+++ src/components/Particles.js
@@ -0,0 +1,11 @@
+import { loadFull } from "tsparticles";
+
+function Particles() {
+  return (
+    <>
+      <Particles id="tsparticles" init={""} options={""} />
+    </>
+  );
+}
+
+export default Particles;
